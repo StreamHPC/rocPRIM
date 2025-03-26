4695ddfd569ff640d9c90b693576048adfa7b6b4
Beatriz Navidad Vilches <beatriz@streamhpc.com>
2025-03-26 16:41:51 +0000

    Include tuned configs

--- rocprim/include/rocprim/device/device_search_n_config.hpp
+++ rocprim/include/rocprim/device/device_search_n_config.hpp
@@ -24,6 +24,7 @@
 #include "config_types.hpp"
 #include "detail/config/device_search_n.hpp"
 #include "detail/device_config_helper.hpp"
+#include "detail/config/device_search_n_config.hpp"
 
 BEGIN_ROCPRIM_NAMESPACE
 
